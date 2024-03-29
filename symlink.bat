9ea79f72df6c47f5475c96c081d868a8d1e0c016
Aelfhe1m <aelfhe1m@hotmail.com>
2024-03-29 19:50:14 +0000

    Update symlink.bat

--- symlink.bat
+++ symlink.bat
@@ -1,7 +1,9 @@
 rem mklink /J "P:\RP1\GameData\PuckAerospace" "P:\Git\KSP\PUCK_Aerospace\GameData\PuckAerospace"
 
-mklink /J "P:\RP1\saves\PUCK\Ships\SPH" "P:\Git\KSP\PUCK_Aerospace\CraftFiles\SPH"
-mklink /J "P:\RP1\saves\PUCK\Ships\VAB" "P:\Git\KSP\PUCK_Aerospace\CraftFiles\VAB"
-mklink /J "P:\RP1\Ships\Script" "P:\Git\KSP\PUCK_Aerospace\Script"
+REM mklink /J "P:\RP1\saves\PUCK\Ships\SPH" "P:\Git\KSP\PUCK_Aerospace\CraftFiles\SPH"
+REM mklink /J "P:\RP1\saves\PUCK\Ships\VAB" "P:\Git\KSP\PUCK_Aerospace\CraftFiles\VAB"
+REM mklink /J "P:\RP1\Ships\Script" "P:\Git\KSP\PUCK_Aerospace\Script"
+
+mklink /J "P:\RP1\Screenshots" "P:\Git\KSP\PUCK_Aerospace\Images-and-artwork\Screenshots\New"
 
 pause
